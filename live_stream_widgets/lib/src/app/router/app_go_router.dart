df82883742d2dfaa0e97b4a6d34e3142dcd3eda6
PationPrime <pationprime@gmail.com>
2025-03-10 23:24:02 +0300

    web app hosting fix

--- live_stream_widgets/lib/src/app/router/app_go_router.dart
+++ live_stream_widgets/lib/src/app/router/app_go_router.dart
@@ -2,30 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../../pages/pages.dart';
 import 'navigation_path.dart';
-// import 'dart:html' as html;
 
-// class GoRouterRefreshStream extends ChangeNotifier {
-//   GoRouterRefreshStream() {
-//     html.window.onPopState.listen((event) {
-//       notifyListeners();
-//     });
-//   }
-// }
-
-final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+final navigatorKey = GlobalKey<NavigatorState>();
 
 class AppGoRouter {
   static final router = GoRouter(
-    // navigatorKey: navigatorKey,
+    navigatorKey: navigatorKey,
     initialLocation: '/',
     errorBuilder: (context, state) => NotFound404(),
-    // refreshListenable: GoRouterRefreshStream(),
     routes: [
       GoRoute(
         name: NavigationPath.root,
         path: NavigationPath.root,
-        // redirect: (context, state) => '/${NavigationPath.main}',
-
         builder: (context, state) {
           final queryParameters = state.uri.queryParameters;
           final bridgeUrl = queryParameters['bridgeUrl'];
@@ -44,27 +32,6 @@ class AppGoRouter {
           );
         },
       ),
-      // GoRoute(
-      //   name: NavigationPath.main,
-      //   path: '/${NavigationPath.main}',
-      //   builder: (context, state) {
-      //     final queryParameters = state.uri.queryParameters;
-      //     final bridgeUrl = queryParameters['bridgeUrl'];
-
-      //     if (bridgeUrl == null) {
-      //       return PathParametersError(
-      //         pagePath: '/${NavigationPath.main}',
-      //         queryParameters: {
-      //           'bridgeUrl': 'String',
-      //         },
-      //       );
-      //     }
-
-      //     return Welcome(
-      //       bridgeUrl: bridgeUrl,
-      //     );
-      //   },
-      // ),
       GoRoute(
         name: NavigationPath.subsGlass,
         path: '/${NavigationPath.subsGlass}',
